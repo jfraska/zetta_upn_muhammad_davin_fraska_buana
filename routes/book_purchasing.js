7f0ec5f5b2db0cde1e1f3cec55aa9a4426e6e4bb
Fraska <58796786+fraska20@users.noreply.github.com>
2023-04-09 15:17:34 +0700

    update

--- routes/book_purchasing.js
+++ routes/book_purchasing.js
@@ -2,59 +2,65 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/purchaseBook', (req, res) => {
-    let { title, author, price, discountPercentage, taxPercentage, stock, amountPurchased, creditTerm } = req.body;   
+async function calculateTermPrice(bookPrice, discountPercentage, taxPercentage, creditTerm, totalPurchased) {
+  const discount = bookPrice * (discountPercentage / 100);
+  const discountedPrice = bookPrice - discount;
+  const subtotal = discountedPrice * totalPurchased;
+  const taxAmount = subtotal * (taxPercentage / 100);
+  const totalPrice = subtotal + taxAmount;
 
-    if (amountPurchased > stock) {
-        res.status(400).json({ message: `Not enough stock available for ${title}.` });
-        return;
-    }
+  return totalPrice / creditTerm;
+}
 
-    let totalPurchased = 0;
-    for (let i = 0; i < amountPurchased; i++) {
-        if (stock <= 0) {
-            res.status(400).json({ message: `Cannot purchase more ${title}. Stock is exhausted.` });
-            break;
-        }
+router.post('/purchaseBook', async (req, res) => {
+  let { title, author, price, discountPercentage, taxPercentage, stock, amountPurchased, creditTerm, additionalPrice} = req.body;   
 
-        totalPurchased++;
-        stock--;
-    }
+  if (amountPurchased > stock) {
+      res.status(400).json({ message: `Not enough stock available for ${title}.` });
+      return;
+  }
 
-    const discount = price * (discountPercentage / 100);
-    const discountedPrice = price - discount;
-    const subtotal = discountedPrice * totalPurchased;
-    const taxAmount = subtotal * (taxPercentage / 100);
-    const totalPrice = subtotal + taxAmount;
+  let totalPurchased = 0;
+  for (let i = 0; i < amountPurchased; i++) {
+      if (stock <= 0) {
+          res.status(400).json({ message: `Cannot purchase more ${title}. Stock is exhausted.` });
+          break;
+      }
 
-    const pricePerCredit = totalPrice / creditTerm;
-    const creditDue = Array.from({ length: creditTerm }, (_, i) => {
-        return {
-          month: i + 1,
-          price: (i === creditTerm - 1) ? totalPrice - (pricePerCredit * (creditTerm - 1)) : pricePerCredit
-        };
-    });
+      totalPurchased++;
+      stock--;
+  }
 
-    res.json({
-        bookDetails: {
-          title,
-          author,
-          price,
-          discountPercentage,
-          taxPercentage,
-          stock,
-          amountPurchased,
-        },
-        paymentDetails: {
-          discountedPrice,
-          subtotal,
-          taxAmount,
-          totalPrice,
-          creditTerm,
-          creditDue,
-        },
-        canPurchaseMore: stock > 0,
-    });
+  let pricePerCredit = await calculateTermPrice(price, discountPercentage, taxPercentage, creditTerm, totalPurchased);
+  if (additionalPrice) {
+    pricePerCredit += additionalPrice;
+  }
+
+  const totalPrice = pricePerCredit * creditTerm;
+  const creditDue = Array.from({ length: creditTerm }, (_, i) => {
+      return {
+        month: i + 1,
+        price: (i === creditTerm - 1) ? totalPrice - (pricePerCredit * (creditTerm - 1)) : pricePerCredit
+      };
+  });
+
+  res.json({
+      bookDetails: {
+        title,
+        author,
+        price,
+        discountPercentage,
+        taxPercentage,
+        stock,
+        amountPurchased,
+      },
+      paymentDetails: {
+        totalPrice,
+        creditTerm,
+        creditDue,
+      },
+      canPurchaseMore: stock > 0,
+  });
 
 });
   
